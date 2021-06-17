39b393a152848f363b7a79472bebe86360d0a6cd
Leandro <leandrotorres73@gmail.com>
2021-06-17 10:42:55 -0300

    remove echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -61,7 +61,6 @@ function installPipRequirements(){
 }
 
 function runCdk(){
-	echo "${CDK_GITHUB_ACCESS_TOKEN}"
 	if [ -n "${CDK_GITHUB_ACCESS_TOKEN}" ]; then
 		echo "Setting CDK Access Token"
 		git config --global url."https://${CDK_GITHUB_ACCESS_TOKEN}@github.com/".insteadOf  https://github.com/
